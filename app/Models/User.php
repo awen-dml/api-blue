cee8e3a51143d19926f1ff55855c96909c04d19a
Awen <awendml@gmail.com>
2026-06-05 15:44:27 +0800

    feat: add Store model with migration and update User model relationships with fingerprint integration documentation

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function store()
+    {
+        return $this->hasOne(Store::class);
+    }
 }
